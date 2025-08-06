8d8beef2b866f6eda84cf49a8abf18e31d6cbb64
ComoSans <153391626+ComoSans@users.noreply.github.com>
2025-08-06 18:51:42 -0400

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Como Sans'
+description 'Any Vehicle Seat'
+version '1.0.0'
+
+shared_scripts {
+    '@ox_lib/init.lua',
+    'config.lua'
+}
+
+client_scripts {
+    'client.lua'
+}
+
+server_scripts {
+    'server.lua'
+}
